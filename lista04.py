007405fc83d390057a3eea3bfd9b7daf7c9bd741
Higor Morais <49865178+higorifrn@users.noreply.github.com>
2025-06-25 09:54:41 -0300

    criação do arquivo lista04.py com docstring

---
+++ lista04.py
@@ -0,0 +1,17 @@
+"""
+lista04.py
+
+Descrição:
+    Este arquivo contém as soluções da lista de exercício 04 da disciplina
+    Introdução à Lógica e Programação.
+
+Autor(es): 
+    - Fulana de Beltrano
+    - Cicrano de Teutrana
+    
+Data: 25/06/2025
+"""
+
+# LEIA-ME
+#    Inclua as soluções de todas as questões nesse arquivo
+#    Faça as atualizações necessárias à docstring acima conforme 
